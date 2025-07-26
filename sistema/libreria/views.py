f173c0bc8170371be38af9d2dd055af895f0ac64
Felipe Sandoval <afsandovalhiguera@gmail.com>
2025-07-26 11:51:59 -0500

    Pagina Inicio

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a la Librería</h1>")
+    return render(request,'paginas/inicio.html')
 
 def nosotros(request):
     return render(request,'paginas/nosotros.html')
 
 def libros(request):
     return render(request,'libros/index.html')
+
